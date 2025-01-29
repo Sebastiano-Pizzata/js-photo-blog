6c49f1e977996659fd6c2c96b5b73198945a1aac
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-29 16:33:25 +0100

    Add: media quearies and js

---
+++ main.js
@@ -0,0 +1,19 @@
+const card = document.getElementById("carte");
+
+fetch("https://lanciweb.github.io/demo/api/pictures/",{method: "GET"})
+.then(response => response.json())
+.then(data => {
+    data.forEach(element => {
+        card.innerHTML += `<div class="card mt-4 ms-4 me-4 col-md-4 col-lg-3">
+            <img src='${element.url}' alt='${element.url}'>
+            <div class="card-body">
+              <span>'${element.date}'</span>
+              <p class="card-text"><strong>'${element.title}'</strong></p>
+            </div>
+          </div>`
+    });
+}
+)
+.catch(error => {
+console.error(error);
+} );
