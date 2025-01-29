a2d6f32d5db8203d3a077039058da12816429f18
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-29 20:02:20 +0100

    add: hover effect

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ fetch("https://lanciweb.github.io/demo/api/pictures/",{method: "GET"})
 .then(response => response.json())
 .then(data => {
     data.forEach(element => {
-        card.innerHTML += `<div class="card mt-4 ms-4 me-4 col-md-4 col-lg-3">
+        card.innerHTML += `<div class="card mt-4 ms-4 me-4 col-md-4 col-lg-3 rotazione">
             <img src='${element.url}' alt='${element.url}'>
             <div class="card-body">
               <span>'${element.date}'</span>
