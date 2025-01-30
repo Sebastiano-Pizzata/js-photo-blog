4e992b56acc093da88184a5d65e1dc74b882ef35
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-30 12:13:08 +0100

    Add: pin

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const card = document.getElementById("carte");
 
-fetch("https://lanciweb.github.io/demo/api/pictures/",{method: "GET"})
-.then(response => response.json())
-.then(data => {
+fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
+  .then(response => response.json())
+  .then(data => {
     data.forEach(element => {
-        card.innerHTML += `<div class="card mt-4 ms-4 me-4 col-md-4 col-lg-3 rotazione">
+      card.innerHTML += `<div class="card p-2 mt-4 ms-4 me-4 col-md-4 col-lg-3 rotazione ">
+            <img width="25px" class="position-absolute top-0 start-50 translate-middle" src="./assets_day1/img/pin.svg" alt="pin">
             <img src='${element.url}' alt='${element.url}'>
             <div class="card-body">
               <span>'${element.date}'</span>
@@ -12,8 +13,8 @@ fetch("https://lanciweb.github.io/demo/api/pictures/",{method: "GET"})
             </div>
           </div>`
     });
-}
-)
-.catch(error => {
-console.error(error);
-} );
+  }
+  )
+  .catch(error => {
+    console.error(error);
+  });
