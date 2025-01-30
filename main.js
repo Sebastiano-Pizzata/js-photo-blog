dcd82205f9b8a95192c122fcca4ac0a0b6aa6148
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-30 20:47:49 +0100

    Add: overlay to sub

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
   .then(response => response.json())
   .then(data => {
     data.forEach(element => {
-      card.innerHTML += `<div class="card p-2 mt-4 ms-4 me-4 col-md-4 col-lg-3 rotazione ">
+      card.innerHTML += `<div class="card p-2 mt-4 ms-4 me-4 col-md-4 col-lg-3 rotazione">
             <img width="25px" class="position-absolute top-0 start-50 translate-middle" src="./assets_day1/img/pin.svg" alt="pin">
             <img src='${element.url}' alt='${element.url}'>
             <div class="card-body">
@@ -12,6 +12,7 @@ fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
               <p class="card-text"><strong>'${element.title}'</strong></p>
             </div>
           </div>`
+
     });
   }
   )
